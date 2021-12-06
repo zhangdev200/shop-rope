a6be0360ae813042c648143508d000f821e93082
stormfeng <2311997861@qq.com>
2021-12-06 15:12:28 +0800

    UserService

--- shoppingserver/src/main/java/com/javaweb/shopping/controller/LoginRegController.java
+++ shoppingserver/src/main/java/com/javaweb/shopping/controller/LoginRegController.java
@@ -1,6 +1,16 @@
 package com.javaweb.shopping.controller;
 
-public class LoginRegController {
+import com.javaweb.shopping.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class LoginRegController {
+    @Autowired
+    UserService userService;
+    @RequestMapping("/login")
+    public void login(){
+
+    }
 }
-“
