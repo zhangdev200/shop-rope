97e4e39c4ecc9e4a2e0fd2e006c2d1dff1a22c7f
stormfeng <2311997861@qq.com>
2021-12-11 22:18:55 +0800

    登录接口

--- shoppingserver/src/main/java/com/javaweb/shopping/service/UserService.java
+++ shoppingserver/src/main/java/com/javaweb/shopping/service/UserService.java
@@ -1,4 +1,11 @@
-package com.javaweb.shopping.service.impl;
+package com.javaweb.shopping.service;
+
+import com.javaweb.shopping.vo.ResultVO;
 
 public interface UserService {
+    //用户注册
+    public ResultVO userResgit(String name, String pwd);
+
+    //用户登录
+    public ResultVO checkLogin(String name, String pwd);
 }
