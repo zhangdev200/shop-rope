2a6f8f914c93a527dbb3d8cb911f2c2237a6d6c2
stormfeng <2311997861@qq.com>
2021-12-13 14:02:10 +0800

    登录注册接口实现

--- shoppingserver/src/main/java/com/javaweb/shopping/controller/CheckTokenInterceptor.java
+++ shoppingserver/src/main/java/com/javaweb/shopping/controller/CheckTokenInterceptor.java
@@ -29,7 +29,7 @@ public class CheckTokenInterceptor implements HandlerInterceptor {
         }else{
             try {
                 JwtParser parser = Jwts.parser();
-                parser.setSigningKey("QIANfeng6666"); //解析token的SigningKey必须和生成token时设置密码一致
+                parser.setSigningKey("xiaofeng"); //解析token的SigningKey必须和生成token时设置密码一致
                 //如果token正确（密码正确，有效期内）则正常执行，否则抛出异常
                 Jws<Claims> claimsJws = parser.parseClaimsJws(token);
                 return true;
--- shoppingserver/src/test/java/com/javaweb/shopping/dao/UserMapperTest.java
+++ shoppingserver/src/test/java/com/javaweb/shopping/dao/UserMapperTest.java
@@ -1,6 +1,7 @@
 package com.javaweb.shopping.dao;
 
 import com.javaweb.shopping.ShoppingApplication;
+import com.javaweb.shopping.entity.User;
 import com.javaweb.shopping.mapper.UserMapper;
 import com.javaweb.shopping.service.UserService;
 import org.junit.Test;
@@ -17,5 +18,10 @@ public class UserMapperTest {
     @Test
     public void test(){
         System.out.println(userMapper);
+        User user = new User();
+        user.setNickname("zxf1");
+        user.setPassword("123456");
+        userMapper.insert(user);
+//        System.out.println(userMapper.selectAll());;
     }
 }
