d8301b5a2e3335cab33d27ec9036fd694a0efbdc
stormfeng <2311997861@qq.com>
2021-12-13 14:56:08 +0800

    增加address列

--- shoppingserver/src/main/java/com/javaweb/shopping/vo/ResultVO.java
+++ shoppingserver/src/main/java/com/javaweb/shopping/vo/ResultVO.java
@@ -1,26 +1,21 @@
 package com.javaweb.shopping.vo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 
-@ApiModel(value = "ResultVO对象",description = "封装接口返回给前端的数据")
 public class ResultVO {
     //响应给前端的状态码
-    @ApiModelProperty(value = "响应状态码",dataType = "int")
     private int code;
 
 
     //响应给前端的提示信息
-    @ApiModelProperty("响应提示信息")
     private String msg;
 
     //响应给前端的数据
-    @ApiModelProperty("响应数据")
     private Object data;
 
     public ResultVO() {
