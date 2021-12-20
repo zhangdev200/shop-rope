cfae5a804ed27f012e028ae34f4c197692fb3db3
HuQuanfu <2248782396@qq.com>
2021-12-20 16:01:19 +0800

    完善接口

--- shoppingserver/src/test/java/com/javaweb/shopping/dao/ShoppingCartTest.java
+++ shoppingserver/src/test/java/com/javaweb/shopping/dao/ShoppingCartTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ShoppingApplication.class)
@@ -19,6 +19,10 @@ public class ShoppingCartTest {
     @Test
     public  void test(){
         List<ShoppingCartVO> shoppingCartVOS = shoppingCartMapper.selectShopcartByUserId(6);
-        System.out.println(shoppingCartVOS.get(0).getContent());
+        List<Integer> list=new LinkedList<>();
+        list.add(1);
+        list.add(6);
+        List<ShoppingCartVO> shoppingCartVOS1 = shoppingCartMapper.selectShopcartByCids(list);
+        System.out.println(shoppingCartVOS1.size());
     }
 }
