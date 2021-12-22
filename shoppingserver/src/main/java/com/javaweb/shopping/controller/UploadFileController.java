c490241a09934114106647384c35381531bd1bae
stormfeng <2311997861@qq.com>
2021-12-22 16:31:56 +0800

    上传文件接口

--- shoppingserver/src/main/java/com/javaweb/shopping/controller/UploadFileController.java
+++ shoppingserver/src/main/java/com/javaweb/shopping/controller/UploadFileController.java
@@ -1,38 +1,73 @@
 package com.javaweb.shopping.controller;
-
 import com.javaweb.shopping.vo.ResStatus;
 import com.javaweb.shopping.vo.ResultVO;
-import org.springframework.util.ClassUtils;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+/**
+ * @author jiashubing
+ * @since 2019/6/25
+ */
 @RestController
-@RequestMapping("/file")
+@RequestMapping("file")
 public class UploadFileController {
 
-
-    @PostMapping("/img")
-    public ResultVO singleFileUpload(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return new ResultVO(ResStatus.NO,"请选择上传文件",null);
-        }
-        try {
-            byte[] bytes = file.getBytes();
-            String s= ClassUtils.getDefaultClassLoader().getResource("static/img").getPath();
-            Path path = Paths.get(s+file.getOriginalFilename());
-            Files.write(path, bytes);
-            return new ResultVO(ResStatus.OK,"上传成功",null);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResultVO(ResStatus.NO,"上传失败",null);
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResultVO uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();//获取文件名
+        fileName = getFileName(fileName);
+        String filepath = getUploadPath();
+        if (!file.isEmpty()) {
+            try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(filepath + File.separator + fileName)))) {
+                out.write(file.getBytes());
+                out.flush();
+                return new ResultVO(ResStatus.OK, fileName,null);
+            } catch (FileNotFoundException e) {
+                return new ResultVO(ResStatus.NO, "上传文件失败 FileNotFoundException：" + e.getMessage(),null);
+            } catch (IOException e) {
+                return new ResultVO(ResStatus.NO, "上传文件失败 IOException：" + e.getMessage(),null);
+            }
+        } else {
+            return new ResultVO(ResStatus.NO, "上传文件失败，文件为空",null);
         }
     }
 
+    /**
+     * 文件名后缀前添加一个时间戳
+     */
+    private String getFileName(String fileName) {
+        int index = fileName.lastIndexOf(".");
+        final SimpleDateFormat sDateFormate = new SimpleDateFormat("yyyymmddHHmmss");  //设置时间格式
+        String nowTimeStr = sDateFormate.format(new Date()); // 当前时间
+        fileName = fileName.substring(0, index) + "_" + nowTimeStr + fileName.substring(index);
+        return fileName;
+    }
+
+    /**
+     * 获取当前系统路径
+     */
+    private String getUploadPath() {
+        File path = null;
+        try {
+            path = new File(ResourceUtils.getURL("classpath:").getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (!path.exists()) path = new File("");
+        File upload = new File(path.getAbsolutePath(), "static/img/");
+        if (!upload.exists()) upload.mkdirs();
+        return upload.getAbsolutePath();
+    }
 }
