0456217d9d6e483ba57e2aff7b8165a292b188e9
86132 <2642440908@qq.com>
2021-12-27 11:23:24 +0800

    最终版本4

--- vueshopping/mall/src/views/DefaultResult.vue
+++ vueshopping/mall/src/views/DefaultResult.vue
@@ -1,8 +1,8 @@
 <template>
     <div style="margin-bottom: 100px">
       <el-carousel :interval="1500" type="card">
-        <el-carousel-item v-for="item in 6" :key="item">
-          <img src="../assets/img.png" style="width: 100%; height: 100%; border-radius: 20px" alt="">
+        <el-carousel-item v-for="item in this.indexImg" :key="item.imgId">
+          <img :src="item.imgUrl" style="width: 100%; height: 100%; border-radius: 20px" alt="">
         </el-carousel-item>
       </el-carousel>
       <div v-for="(category, index) in this.categoryList" :key=index>
@@ -28,12 +28,22 @@ export default {
   name: "DefaultResult",
   data() {
     return {
+      indexImg: null,
       goodsData: new Map,
       categoryList: [],
     }
   },
   methods: {},
   created() {
+    this.$http
+    .get('/index/indeximg')
+    .then(res => {
+      if (res.code === 10000) {
+        this.indexImg = res.data;
+      } else {
+        this.$message.error('未知错误');
+      }
+    })
     this.$http
         .get('/index/category-recommends')
         .then(res => {
